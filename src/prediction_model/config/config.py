ee5475e034d6bccfba3e574727264c0441c85c39
Omar-Karimov <omarkerimov303@gmail.com>
2024-03-20 02:19:01 +0400

    Update config and refactor data_handling

--- src/prediction_model/config/config.py
+++ src/prediction_model/config/config.py
@@ -9,6 +9,7 @@ DATAPATH = os.path.join(PACKAGE_ROOT, "datasets")
 TRAIN_FILE = "train.csv"
 TEST_FILE = "test.csv"
 
+MODEL_NAME = "classification.pkl"
 SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT, "trained_models")
 
 TARGET = "loan_status"
